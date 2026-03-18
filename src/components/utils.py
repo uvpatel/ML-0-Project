696f1a07bb682433ad03ff73f9b6b8ad5278de72
uvpatel <uvpatel7271@gmail.com>
2026-03-18 22:34:55 +0530

    working with hyper parameter training

--- src/components/utils.py
+++ src/components/utils.py
@@ -1,12 +1,11 @@
 import os
 import sys
-
 import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
-
 
 
 def save_object(file_path,obj):
@@ -21,10 +20,18 @@ def save_object(file_path,obj):
         raise CustomException(e, sys)
 
 
-def evaluate_model(X, y, X_test,y_test,models):
+def evaluate_model(X, y, X_test, y_test, models, param=None):
     try:
         report = {}
+
         for model_name, model in models.items():
+            para = param.get(model_name, {}) if param else {}
+
+            if para:
+                gs = GridSearchCV(model, para, cv=3, n_jobs=-1, verbose=0, refit=True)
+                gs.fit(X, y)
+                model = gs.best_estimator_
+
             model.fit(X, y)
             predictions = model.predict(X_test)
             report[model_name] = r2_score(y_test, predictions)
